9136ae15fc50f115f09c2cc1233aecbdfc1542a1
Leonardo <leo.msouza1706@gmail.com>
2024-07-01 18:47:55 -0300

    Criando a rota de livros

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ import express from 'express';
 
 const app = express();
 
+const livros = [
+    {
+        id:1,
+        titulo: "Como Fazer Amigos e Influenciar Pessoas"
+    },
+    {
+        id:2,
+        titulo: "Os Segredos da Mente Milionária"
+    }
+]
+
 app.get("/", (req, res)=>{
     res.status(200).send("Curso de Node.js!");
+});
+
+app.get("/livros", (req, res)=>{
+    res.status(200).json(livros)
 });
 
 export default app
